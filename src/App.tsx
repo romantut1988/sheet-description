36f64a9f45eb6afa56af6b17e7dd184fee3f1969
Roman <romantut1988@mail.ru>
2023-08-04 16:22:19 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
     const tasks1 = [
         {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
+        {id: 2, title: "JS-TYPESCRIPT", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "React", isDone: false},
     ]
