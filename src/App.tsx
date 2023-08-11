73e681179026b386c82fa2cb3f5d83d53d9335e6
Roman <romantut1988@mail.ru>
2023-08-12 01:45:14 +0300

    restart remove idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import './App.css';
 import {Todolist} from "./Todolist";
 
@@ -10,10 +10,10 @@ function App() {
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "React vs Angular", isDone: false},
+        {id: 4, title: "React", isDone: false},
     ]
     const tasks2 = [
-        {id: 1, title: "Hello world", isDone: true},
+        {id: 1, title: "Hello worlds", isDone: true},
         {id: 2, title: "I am Happy", isDone: false},
         {id: 3, title: "Yo", isDone: false}
     ]
